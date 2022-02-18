cb2c80eeb01383bdd426607b336668b803a627bf
keerthan2 <keerthan2@gmail.com>
2022-02-18 18:18:53 +0530

    added clustering and noise removal

--- utils/plyfile.py
+++ utils/plyfile.py
@@ -22,10 +22,10 @@ import numpy as _np
 from sys import byteorder as _byteorder
 
 
-try:
-    _range = xrange
-except NameError:
-    _range = range
+# try:
+#     _range = xrange
+# except NameError:
+#     _range = range
 
 
 # Many-many relation
@@ -661,7 +661,7 @@ class PlyElement(object):
         """
         self._data = _np.empty(self.count, dtype=self.dtype(byte_order))
 
-        for k in _range(self.count):
+        for k in range(self.count):
             for prop in self.properties:
                 try:
                     self._data[prop.name][k] = \
